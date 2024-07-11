f3eab930acbc064d417da41ddc71e13fdd5fb20f
RanitManik <138437760+RanitManik@users.noreply.github.com>
2024-07-12 01:01:50 +0530

    added project 01

---
+++ 01. Gemini Clone/README.md
@@ -0,0 +1,47 @@
+# Gemini Clone
+
+Gemini Clone is a React-based project designed to replicate the functionality of Google Gemini AI chatbot, leveraging
+the Google Gemini API for enhanced conversational interactions.
+
+## Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/RanitManik/Gemini-Clone.git
+   ```
+
+2. Navigate to the project directory:
+
+   ```bash
+   cd Gemini-Clone
+   ```
+
+3. Install dependencies:
+
+   ```bash
+   npm install
+   ```
+
+4. Create a `.env.local` file in the root of the project and add your Google Gemini API key:
+
+   ```plaintext
+   GEMINI_API_KEY="YOUR_GEMINI_API_KEY"
+   ```
+
+5. Start the development server:
+
+   ```bash
+   vite
+   ```
+
+## Usage
+
+After installation, open your browser and navigate to `http://localhost:5173/` to access the Gemini Clone application.
+You can start interacting with the Gemini model through the chat interface provided.
+
+---
+
+<p align=center>Feel free to contact me at <a href="mailto:ranitmanik.dev@gmail.com">ranitmanik.dev@gmail.com</a> for any queries or further
+information about the project.</p>
+
---
+++ 01. Gemini Clone/src/components/Main/Main.jsx
@@ -0,0 +1,115 @@
+import React, {useContext, useEffect, useRef, useState} from 'react';
+import './Main.css';
+import {assets} from "../../assets/assets.js";
+import {Context} from "../../context/Context.jsx";
+
+const Main = () => {
+    const {onSent, recentPrompt, showResult, loading, resultData, setInput, input} = useContext(Context);
+    const resultRef = useRef(null);
+    const [rows, setRows] = useState(1);
+
+    useEffect(() => {
+        const updateRows = () => {
+            if (window.innerWidth <= 600) {
+                setRows(2);
+            } else {
+                setRows(1);
+            }
+        };
+
+        updateRows();
+        window.addEventListener('resize', updateRows);
+        return () => window.removeEventListener('resize', updateRows);
+    }, []);
+
+    useEffect(() => {
+        if (resultRef.current) {
+            resultRef.current.scrollTop = resultRef.current.scrollHeight;
+        }
+    }, [resultData]);
+
+    return (
+        <main className="main">
+            <nav className="nav">
+                <p>Gemini</p>
+                <img src={assets.user_icon} alt=""/>
+            </nav>
+            <div className="main-container">
+
+                {!showResult
+                    ? <>
+                        <div className="greet">
+                            <p><span>Hello, Dev</span></p>
+                            <p>How can I help you today?</p>
+                        </div>
+                        <div className="cards">
+                            <div className="card"
+                                 onClick={() => setInput("Suggest beautiful places to see on an upcoming road trip")}>
+                                <p>Suggest beautiful places to see on an upcoming road trip</p>
+                                <img src={assets.compass_icon} alt=""/>
+                            </div>
+                            <div className="card"
+                                 onClick={() => setInput("Briefly summarize this concept: urban planning")}>
+                                <p>Briefly summarize this concept: urban planning</p>
+                                <img src={assets.bulb_icon} alt=""/>
+                            </div>
+                            <div className="card"
+                                 onClick={() => setInput("Brainstorm team bonding activities for our work retreat")}>
+                                <p>Brainstorm team bonding activities for our work retreat</p>
+                                <img src={assets.message_icon} alt=""/>
+                            </div>
+                            <div className="card" onClick={() => setInput("Tell me about React js and React native")}>
+                                <p>Tell me about React js and React native</p>
+                                <img src={assets.code_icon} alt=""/>
+                            </div>
+                        </div>
+                    </>
+                    :
+                    <div className='result' ref={resultRef}>
+                        <div className="result-title">
+                            <img src={assets.user_icon} alt=""/>
+                            <p>{recentPrompt}</p>
+                        </div>
+                        <div className="result-data">
+                            <img className="result-data-icon" src={assets.gemini_icon} alt=""/>
+                            {loading ?
+                                <div className='loader'>
+                                    <hr/>
+                                    <hr/>
+                                    <hr/>
+                                </div>
+                                :
+                                <p dangerouslySetInnerHTML={{__html: resultData}}></p>
+                            }
+                        </div>
+                    </div>
+                }
+                <div className="main-bottom">
+                    <div className="search-box">
+                        <textarea rows={rows} onChange={(e) => setInput(e.target.value)}
+                                  onKeyUp={(e) => {
+                                      if (e.key === 'Enter') {
+                                          onSent();
+                                      }
+                                  }}
+                                  value={input}
+                                  type="text"
+                                  placeholder="Enter a prompt here"
+                        />
+                        <div className="icon-container">
+                            <button><img src={assets.gallery_icon} alt=""/></button>
+                            <button><img src={assets.mic_icon} alt=""/></button>
+                            <button type="submit" onClick={() => onSent()}><img src={assets.send_icon} alt=""/></button>
+                        </div>
+                    </div>
+                    <p className="bottom-info">
+                        Gemini may display inaccurate info, including about people, so double-check its responses.
+                        <a href="#">Your privacy and Gemini Apps</a>
+                    </p>
+                </div>
+            </div>
+        </main>
+    );
+}
+
+export default Main;
